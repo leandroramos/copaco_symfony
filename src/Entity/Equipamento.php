4f4186a0e269c0a4ee2fb7e2c397197f0e73e038
Alessandro Costa de Oliveira <alecosta@usp.br>
2018-01-11 03:33:32 -0800

    Create get and set for all fields in entity Equipamento

--- src/Entity/Equipamento.php
+++ src/Entity/Equipamento.php
@@ -40,4 +40,54 @@ class Equipamento
     * @ORM\Column(type="datetimetz")
      */
     private $vencimento;
+
+    public function getPatrimonio()
+    {
+        return $this->patrimonio;
+    }
+
+    public function setPatrimonio($patrimonio)
+    {
+        $this->patrimonio = $patrimonio;
+    }
+
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    public function getMacaddress()
+    {
+        return $this->macaddress;
+    }
+
+    public function setMacaddress($macaddress)
+    {
+        $this->macaddress = $macaddress;
+    }
+
+    public function getLocal()
+    {
+        return $this->local;
+    }
+
+    public function setLocal($local)
+    {
+        $this->local = $local;
+    }
+
+    public function getVencimento()
+    {
+        return $this->vencimento;
+    }
+
+    public function setVencimento($vencimento)
+    {
+        $this->vencimento = $vencimento;
+    }
 }
